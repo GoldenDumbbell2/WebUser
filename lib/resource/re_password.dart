5670eb1b7e04f842bb08e06c0c2c38601f83218e
KockHere <106009305+KockHere@users.noreply.github.com>
2023-05-29 10:43:12 +0700

    repass
    rePasssword

---
+++ lib/resource/re_password.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:webspc/resource/forgot_password.dart';
+import 'register_page.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/rePassScreen';
+  final BuildContext context;
+
+  // ignore: use_key_in_widget_constructors
+  const LoginScreen(this.context);
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginScreen> {
+  final RoundedLoadingButtonController _btnLogin =
+      RoundedLoadingButtonController();
+  TextEditingController newPassController = TextEditingController();
+  TextEditingController confirmPassController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('images/bga.png'),
+            fit: BoxFit.cover,
+          )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Image.asset('images/logo.png'),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'Smart Parking System',
+                style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 480,
+                width: 325,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Log In',
+                      style:
+                          TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: 250,
+                      child: TextFormField(
+                        controller: newPassController,
+                        decoration: InputDecoration(
+                          labelText: 'Type in your new password',
+                          // suffixIcon: Icon(FontAwesomeIcons.envelope,size: 17,),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 250,
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: confirmPassController,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm your new password',
+                          // suffixIcon: Icon(FontAwesomeIcons.eyeSlash,size: 17,),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(left: 130),
+                        child: RichText(
+                          text: TextSpan(
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ForgotPasswordScreen(context)));
+                                },
+                              text: " Forget Password",
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 15)),
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+
+                    Container(
+                      margin: EdgeInsets.only(left: 60, right: 60),
+                      child: RoundedLoadingButton(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.height * 0.055,
+                        child: Text("SIGN IN",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            )),
+                        color: const Color.fromRGBO(20, 160, 240, 1.0),
+                        controller: _btnLogin,
+                        // onPressed: _onLoginPress,
+                        onPressed: () {},
+                        // borderRadius: MediaQuery.of(context).size.height * 0.04,
+                      ),
+                    ),
+                    // ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                        child: RichText(
+                      text: TextSpan(
+                        text: "Do you have account ?",
+                        style: TextStyle(color: Colors.black, fontSize: 15),
+                        children: <TextSpan>[
+                          TextSpan(
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              RegisterScreen(context)));
+                                },
+                              text: " Register",
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 15)),
+                        ],
+                      ),
+                    )),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //  void login() async {
+  //   try{
+  //   Response response = await get(
+  //     Uri.parse('https://spsapiservice.azurewebsites.net/api/TbUsers'),
+  //   );
+
+  //   var data = jsonDecode(response.body);
+
+  // User user = User(email: data['email'], pass: data['pass']);
+
+  // for(int i=0; i< data.size(); i++){
+  //   User user = data.get(i);
+  //   print(user);
+  // }
+
+  // String email = emailController.toString();
+  // String password = passwordController.toString();
+  //   if( email == user.Getemail&& password == user.Getpassword){
+  //     print('ok');
+  //   }else{
+  //     print('fail');
+  //   }
+  // print(data);
+
+  //   }catch(e){
+  //     print(e);
+  //   }
+  //  }
+  // if (email == email1 && password == password1){
+  //   print('Login successfully');
+  // Navigator.pushNamed(
+  //   widget.context,
+  //   HomeScreen.routeName,
+  // );
+  // }else{
+  //   print('fail');
+  // }
+}
